37899c78320dc396487c4fbb82c9dab605ea723a
Edo <e.vanveen@science.ru.nl>
2018-03-27 15:14:11 +0200

    Added fortran compilation script for gnu+f2py3, added comments to scripts for clarification.

--- tipsi/fortran/make_tbpm_clusternodes_ru_nijmegen.sh
+++ tipsi/fortran/make_tbpm_clusternodes_ru_nijmegen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# fortran compilation script for Radboud university Nijmegen FNWI cluster nodes
 source /vol/opt/intelcompilers/intel-2014/composerxe/bin/compilervars.sh intel64 
 f2py3 --fcompiler=intelem --f90flags="-openmp -O3 -march=native -heap-arrays" -L/vol/opt/intelcompilers/intel-2014/composerxe/lib/intel64 -liomp5 -lifcoremt -c tbpm_module.f90 tbpm_f2py.f90 -m tbpm_f2py
--- tipsi/fortran/make_tbpm_gnu95.sh
+++ tipsi/fortran/make_tbpm_gnu95.sh
@@ -1,1 +1,2 @@
+# use gnu95 compiler and f2py
 f2py --fcompiler=gnu95 --f90flags='-march=native -O3 -fopenmp' -lgomp -c tbpm_module.f90 tbpm_f2py.f90 -m tbpm_f2py
