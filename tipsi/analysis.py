Fixed some bugs in DC conductivity calculation and analysis, now testing.

tipsi/analysis.py
@@ -404,9 +404,10 @@ def analyze_corr_DC(config, corr_DOS, corr_DC, \
     energies = energies_DOS[QE_indices]
     dc_prefactor = config.sample['nr_orbitals'] \
                    / config.sample['area_unit_cell']
-
+    
     # get DC conductivity
     DC = np.zeros((2, n_energies))
+    DC_int = np.zeros((2, n_energies, tnr))
     for i in range(2):
         for j in range(n_energies):
 
@@ -414,12 +415,13 @@ def analyze_corr_DC(config, corr_DOS, corr_DC, \
             dosval = DOS[QE_indices[j]]
             dcval = 0.
             for k in range(tnr):
-                W = window_DC(k + 1, tnr)
+                W = window_DC(k, tnr)
                 cexp = np.exp(-1j * k * t_step * en)
                 add_dcv = W * (cexp * corr_DC[i, j, k]).real
                 dcval += add_dcv
-            DC[i, j] = dc_prefactor * t_step * dosval * dcval
-
+                DC_int[i, j, k] = dc_prefactor * t_step * dosval * dcval
+            DC[i, j] = np.amax(DC_int[i, j, :])
+    
     # correct for spin
     if config.generic['correct_spin']:
         DC = 2. * DC