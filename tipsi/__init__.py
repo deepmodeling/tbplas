b61662d0d4a7709ad882094c7aec2d5442bdf6fe
yhli <liyunhai1016@hotmail.com>
2021-11-06 17:18:38 +0800

    Added more stuff in tipsi/__init__.py and removed input.py

--- tipsi/__init__.py
+++ tipsi/__init__.py
@@ -1,1 +1,6 @@
 """Initialization of Tipsi package."""
+from .builder import *
+from .analysis import window_exp, window_exp_ten, window_hanning, Analyzer
+from .config import read_config, Config
+from .solver import Solver
+from .utils import Timer
