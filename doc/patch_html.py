dbc056bf6d04e819f7e0d058d28f1d0d5c07fc12
yhli <liyunhai1016@hotmail.com>
2022-03-18 15:23:41 +0800

    Add ICP to doc

--- doc/patch_html.py
+++ doc/patch_html.py
@@ -3,7 +3,7 @@
 import os
 
 
-def patch_html(html):
+def patch_stat(html):
     patch = """
     <script>
         var _hmt = _hmt || [];
@@ -25,12 +25,29 @@ def patch_html(html):
         content.insert(nl_head, patch)
         with open(html, "w") as f:
             f.writelines(content)
- 
+
+
+def patch_icp(html):
+    icp = """
+    <div align=center>
+        <br>
+            <a href="http://beian.miit.gov.cn/"; target=_blank>鄂ICP备2022004007号</a>
+        </br>
+    </div>\n\n"""
+    with open(html, "r") as f:
+        content = f.readlines()
+        nl_footer = content.index("</footer>\n")
+        content.insert(nl_footer-1, icp)
+
+    with open(html, "w") as f:
+        f.writelines(content)
+
 
 def main():
     html_files = os.popen("find build/html -name '*.html'").readlines()
     for html in html_files:
-        patch_html(html.rstrip("\n"))
+        patch_stat(html.rstrip("\n"))
+    patch_icp("build/html/index.html")
 
 
 if __name__ == "__main__":
