023c51ed9893e16625914fab33826820baa035fc
Yunhai Li <liyunhai1016@hotmail.com>
2022-05-12 16:15:08 +0800

    Removed redundant imports

--- tbplas/__init__.py
+++ tbplas/__init__.py
@@ -5,6 +5,5 @@ from .lindhard import Lindhard
 from .analysis import window_exp, window_exp_ten, window_hanning, Analyzer
 from .config import read_config, Config
 from .solver import Solver
-from .lindhard import Lindhard
 from .utils import Timer
 from .visual import Visualizer
