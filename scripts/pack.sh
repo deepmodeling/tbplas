a499161bcbd1ed4711e7a9a4c854043cafd2348a
Yunhai Li <liyunhai1016@hotmail.com>
2022-08-31 21:14:20 +0800

    Added script for packing the source code

---
+++ scripts/pack.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+
+items="CITING.rst  config  doc  examples  LICENSE.rst  README.rst  \
+requirements.txt  scripts  setup.cfg  setup.py  tbplas  tests"
+
+# Copy items to destination
+test -d tmp/tbplas && rm -rf tmp/tbplas
+mkdir -p tmp/tbplas
+for item in $items; do
+    cp -r $item tmp/tbplas
+done
+
+# Remove unnecessary files
+cd tmp
+for kind in pyc mod o so; do
+    find . -name *.$kind | xargs rm -rf
+done
+
+for kind in __pycache__ sim_data; do
+    find . -name $kind | xargs rm -rf
+done
+
+# Compress
+tar -czf tbplas.tar.gz tbplas
