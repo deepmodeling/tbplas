c63c0687d830e7044e1147a874edd6c8beebd638
Yunhai Li <liyunhai1016@hotmail.com>
2023-05-08 17:00:14 +0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import os
-import configparser
 
 
 # Shared package information by FORTRAN and C extensions
@@ -24,74 +23,66 @@ full_packages = ['tbplas', 'tbplas.adapter', 'tbplas.base', 'tbplas.builder',
 c_packages = ['tbplas.cython']
 f_packages = set(full_packages).difference(c_packages)
 
-# FORTRAN extension
-try:
-    import numpy as np
+
+# FORTRAN extensions
+def f_setup():
     from numpy.distutils.core import setup, Extension
-except ImportError:
-    print('Error: numpy not found. Installation failed.')
-    exit()
 
-# Generate the interface
-f90_dir = 'tbplas/fortran'
-os.system(f'f2py -h {f90_dir}/f2py.pyf -m f2py --overwrite-signature '
-          f'{f90_dir}/tbpm.f90 {f90_dir}/analysis.f90 {f90_dir}/lindhard.f90')
+    # Generate the interface
+    f90_dir = 'tbplas/fortran'
+    f90_export = ['tbpm.f90', 'analysis.f90', 'lindhard.f90']
+    f2py_cmd = f'f2py -h {f90_dir}/f2py.pyf -m f2py --overwrite-signature'
+    for src in f90_export:
+        f2py_cmd += f' {f90_dir}/{src}'
+    os.system(f2py_cmd)
 
-# NOTE: DO NOT change the ordering of f90 files. Otherwise, the
-# dependencies will be violated the compilation will fail.
-f90_files = ['const.f90', 'math.F90', 'csr.F90', 'fft.F90', 'random.f90',
-             'propagation.f90', 'kpm.f90', 'funcs.f90',
-             'tbpm.f90', 'analysis.f90', 'lindhard.f90']
-f_sources = [f'{f90_dir}/{file}' for file in f90_files]
-f_sources.insert(0, f'{f90_dir}/f2py.pyf')
+    # Define the extensions
+    # DO NOT change the ordering of f90 files. Otherwise, the dependencies will
+    # be violated the compilation will fail.
+    f_sources = ['f2py.pyf', 'const.f90', 'math.F90', 'csr.F90', 'fft.F90',
+                 'random.f90', 'propagation.f90', 'kpm.f90', 'funcs.f90',
+                 'tbpm.f90', 'analysis.f90', 'lindhard.f90']
+    f_sources = [f'{f90_dir}/{file}' for file in f_sources]
+    f_extensions = [
+        Extension(name='tbplas.fortran.f2py', sources=f_sources)
+    ]
 
-f_extensions = [
-    Extension(
-        name='tbplas.fortran.f2py',
-        sources=f_sources
-    )
-]
-
-setup(
-    **pkg_info,
-    packages=f_packages,
-    ext_modules=f_extensions,
-)
+    # Run setup
+    setup(**pkg_info, packages=f_packages, ext_modules=f_extensions)
 
 
-# C Extension
-try:
+# C Extensions
+def c_setup():
+    import configparser
     from setuptools import Extension, setup
-except ImportError:
-    print('Error: setuptools not found. Installation failed.')
-    exit()
-try:
     from Cython.Build import cythonize
-except ImportError:
-    print('Error: Cython not found. Installation failed.')
-    exit()
+    import numpy as np
 
-# Detect compiler from setup.cfg
-config = configparser.ConfigParser()
-config.read('setup.cfg')
-if 'config_cc' in config.sections():
-    cc = config.get('config_cc', 'compiler')
-else:
-    cc = 'unix'
-if cc == 'intelem':
-    os.environ['CC'] = 'icc'
-    os.environ['LDSHARED'] = 'icc -shared'
+    # Detect compiler from setup.cfg
+    config = configparser.ConfigParser()
+    config.read('setup.cfg')
+    if 'config_cc' in config.sections():
+        cc = config.get('config_cc', 'compiler')
+    else:
+        cc = 'unix'
+    if cc == 'intelem':
+        os.environ['CC'] = 'icc'
+        os.environ['LDSHARED'] = 'icc -shared'
 
-ext_names = ['primitive', 'super', 'sample', 'lindhard']
-c_extensions = [
-    Extension(name=f"tbplas.cython.{name}",
-              sources=[f"tbplas/cython/{name}.pyx"],
-              include_dirs=[np.get_include()])
-    for name in ext_names
-]
+    # Define the extensions
+    ext_names = ['primitive', 'super', 'sample', 'lindhard']
+    c_extensions = [
+        Extension(name=f"tbplas.cython.{name}",
+                  sources=[f"tbplas/cython/{name}.pyx"],
+                  include_dirs=[np.get_include()])
+        for name in ext_names
+    ]
 
-setup(
-    **pkg_info,
-    packages=c_packages,
-    ext_modules=cythonize(c_extensions),
-)
+    # Run setup
+    setup(**pkg_info, packages=c_packages, ext_modules=cythonize(c_extensions))
+
+
+if __name__ == "__main__":
+    # FORTRAN extensions should go first. Otherwise, the compilation will fail.
+    f_setup()
+    c_setup()
