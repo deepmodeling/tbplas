7385538a6cfa821c747dd0e819b0ac95f86c4fd6
Yunhai Li <liyunhai1016@hotmail.com>
2023-06-08 17:52:19 +0800

    Add script to extract api

---
+++ scripts/get_api.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+from inspect import ismodule
+
+import tbplas as tb
+
+
+# Get initial labels
+attr_dict = tb.__dict__
+labels = attr_dict.keys()
+
+# Filter builtin names
+labels = set([_ for _ in labels if _[0] != "_"])
+
+# Filter Exceptions
+labels = labels.difference(dir(tb.exceptions))
+
+# Filter physical constants
+labels = labels.difference(dir(tb.constants))
+
+# Echo
+labels = sorted(labels)
+for label in labels:
+    attr = attr_dict[label]
+    if not ismodule(attr):
+        print(label)
