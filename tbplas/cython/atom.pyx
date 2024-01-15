6e1db54ab4c0c766c86ca465d6fcdebce39ce4a0
Yunhai Li <liyunhai1016@hotmail.com>
2024-01-15 10:18:13 +0800

    Efficiency enhance for atom.pyx

--- tbplas/cython/atom.pyx
+++ tbplas/cython/atom.pyx
@@ -13,7 +13,7 @@ import numpy as np
 
 
 cdef double _factorial(int n):
-    """Evaluate factorial of n."""
+    """Evaluate the factorial of n."""
     cdef double y = 1.0
     cdef int i = 1
     for i in range(1, n+1):
@@ -21,8 +21,8 @@ cdef double _factorial(int n):
     return y
 
 
-cdef double _norm_factor(int z, int n, int l):
-    """Evaluate renormalization factor of Rnl(r)."""
+cdef double _rnl_factor(int z, int n, int l):
+    """Evaluate the renormalization factor of Rnl(r)."""
     cdef double a, b, c
     a = 2.0 / (n**2 * _factorial(2*l+1))
     b = sqrt(_factorial(n+l) / _factorial(n-l-1))
@@ -31,7 +31,7 @@ cdef double _norm_factor(int z, int n, int l):
 
 
 cdef double _kummer(int alpha, int gamma, double ita):
-    """Evaluate Kummer function in Rnl(r)."""
+    """Evaluate the Kummer function in Rnl(r)."""
     cdef double _sum = 1.0
     cdef double ck = 1.0
     cdef int k = 1
@@ -42,71 +42,119 @@ cdef double _kummer(int alpha, int gamma, double ita):
 
 
 cdef double _radial(int z, int n, int l, double r):
-    """Evaluate Rnl(r) of atomic wave function."""
-    cdef double ita, factor, a, b, c
+    """Evaluate the un-normalized Rnl(r)."""
+    cdef double ita, a, b, c
     ita = 2.0 * z / n * r
-    factor = _norm_factor(z, n, l)
     a = exp(-0.5 * ita)
     b = ita**l
     c = _kummer(-n+l+1, 2*l+2, ita)
-    return factor * a * b * c
+    return a * b * c
 
 
 cdef double _coeff(int a, int b, int c, int d):
-    """Evaluate the coeffcient term in ylm(x, y, z)."""
+    """Evaluate the coefficient term in ylm(x, y, z)."""
     return a / b * sqrt(c / (d * pi))
+
+
+cdef double _ylm_factor(int l, int m):
+    """Evaluate the renormalization factor of ylm(x, y, z)."""
+    cdef double factor
+    if l == 0:
+        factor = _coeff( 1, 2, 1, 1 )
+    elif l == 1:
+        if m == -1:  # py
+            factor = _coeff( 1, 1, 3, 4 )
+        elif m == 0:  # pz
+            factor = _coeff( 1, 1, 3, 4 )
+        elif m == 1:  # px
+            factor = _coeff( 1, 1, 3, 4 )
+        else:
+            factor = 0.0
+    elif l == 2:
+        if m == -2:  # dxy
+            factor = _coeff( 1, 2, 15, 1 )
+        elif m == -1:  # dyz
+            factor = _coeff( 1, 2, 15, 1 )
+        elif m == 0:  # dz^2
+            factor = _coeff( 1, 4,  5, 1 )
+        elif m == 1:  # dxz
+            factor = _coeff( 1, 2, 15, 1 )
+        elif m == 2:  # d(x^2-y^2)
+            factor = _coeff( 1, 4, 15, 1 )
+        else:
+            factor = 0.0
+    elif l == 3:
+        if m == -3:  # fy(3x^2-y^2)
+            factor = _coeff( 1, 4,  35, 2 )
+        elif m == -2:  # fxyz
+            factor = _coeff( 1, 2, 105, 1 )
+        elif m == -1:  # fyz^2
+            factor = _coeff( 1, 4,  21, 2 )
+        elif m ==  0:  # fz^3
+            factor = _coeff( 1, 4,   7, 1 )
+        elif m ==  1:  # fxz^2
+            factor = _coeff( 1, 4,  21, 2 )
+        elif m ==  2:  # fz(x^2-y^2)
+            factor = _coeff( 1, 4, 105, 1 )
+        elif m ==  3:  # fx(x^2-3y^2)
+            factor = _coeff( 1, 4,  35, 2 )
+        else: 
+            factor = 0.0
+    else:
+        factor = 0.0
+    return factor
 
 
 cdef double _ylm(int l, int m, double x, double y, double z, double r):
     """
-    Evaluate real spherical harmonics ylm(x, y, z).
+    Evaluate the un-normalized real spherical harmonics ylm(x, y, z).
 
     Reference:
     https://en.wikipedia.org/wiki/Table_of_spherical_harmonics#Real_spherical_harmonics
     """
     cdef double denorm, ylm
     if l == 0:
-        ylm = _coeff( 1, 2, 1, 1 )
+        ylm = 1.0
     elif l == 1:
         denorm = r
         if m == -1:  # py
-            ylm = _coeff( 1, 1, 3, 4 ) / denorm * y
+            ylm = 1.0 / denorm * y
         elif m == 0:  # pz
-            ylm = _coeff( 1, 1, 3, 4 ) / denorm * z
+            ylm = 1.0 / denorm * z
         elif m == 1:  # px
-            ylm = _coeff( 1, 1, 3, 4 ) / denorm * x
+            ylm = 1.0 / denorm * x
         else:
             ylm = 0.0
     elif l == 2:
         denorm = r**2
         if m == -2:  # dxy
-            ylm = _coeff( 1, 2, 15, 1 ) / denorm * ( x*y )
+            ylm = 1.0 / denorm * ( x*y )
         elif m == -1:  # dyz
-            ylm = _coeff( 1, 2, 15, 1 ) / denorm * ( y*z )
+            ylm = 1.0 / denorm * ( y*z )
         elif m == 0:  # dz^2
-            ylm = _coeff( 1, 4,  5, 1 ) / denorm * ( 2*z*z - x*x - y*y )
+            ylm = 1.0 / denorm * ( 2*z*z - x*x - y*y )
         elif m == 1:  # dxz
-            ylm = _coeff( 1, 2, 15, 1 ) / denorm * ( z*x )
+            ylm = 1.0 / denorm * ( z*x )
         elif m == 2:  # d(x^2-y^2)
-            ylm = _coeff( 1, 4, 15, 1 ) / denorm * ( x*x - y*y )
+            ylm = 1.0 / denorm * ( x*x - y*y )
         else:
             ylm = 0.0
     elif l == 3:
         denorm = r**3
         if m == -3:  # fy(3x^2-y^2)
-            ylm = _coeff( 1, 4,  35, 2 ) / denorm * y * ( 3*x*x -y*y )
+            ylm = 1.0 / denorm * y * ( 3*x*x -y*y )
         elif m == -2:  # fxyz
-            ylm = _coeff( 1, 2, 105, 1 ) / denorm * ( x*y*z )
+            ylm = 1.0 / denorm * ( x*y*z )
         elif m == -1:  # fyz^2
-            ylm = _coeff( 1, 4,  21, 2 ) / denorm * y * ( 4*z*z - x*x - y*y )
+            ylm = 1.0 / denorm * y * ( 4*z*z - x*x - y*y )
         elif m ==  0:  # fz^3
-            ylm = _coeff( 1, 4,   7, 1 ) / denorm * z * ( 2*z*z - 3*x*x - 3*y*y )
+            ylm = 1.0 / denorm * z * ( 2*z*z - 3*x*x - 3*y*y )
         elif m ==  1:  # fxz^2
-            ylm = _coeff( 1, 4,  21, 2 ) / denorm * x * ( 4*z*z - x*x - y*y )
+            ylm = 1.0 / denorm * x * ( 4*z*z - x*x - y*y )
         elif m ==  2:  # fz(x^2-y^2)
-            ylm = _coeff( 1, 4, 105, 1 ) / denorm * z * ( x*x - y*y )
+            ylm = 1.0 / denorm * z * ( x*x - y*y )
         elif m ==  3:  # fx(x^2-3y^2)
-            ylm = _coeff( 1, 4,  35, 2 ) / denorm * x * ( x*x - 3*y*y )
+            ylm = 1.0 / denorm * x * ( x*x - 3*y*y )
         else: 
             ylm = 0.0
     else:
@@ -138,12 +186,13 @@ def set_cube(double [::1] c0, int[::1] qn, double[::1] cube_origin,
     """
     cdef double xmin, ymin, zmin
     cdef double dx, dy, dz, dx2, dx2y2
-    cdef double r, gridval
+    cdef double r, gridval, factor
     cdef int z, n, l, m
     cdef int i, j, k
 
     xmin, ymin, zmin = cube_origin[0], cube_origin[1], cube_origin[2]
     z, n, l, m = qn[0], qn[1], qn[2], qn[3]
+    factor = _rnl_factor(z, n, l) * _ylm_factor(l, m)
 
     for i in range(num_grid[0]):
         dx = xmin + res * i - c0[0]
@@ -157,5 +206,5 @@ def set_cube(double [::1] c0, int[::1] qn, double[::1] cube_origin,
                 if r < 1.0e-7:
                     gridval = 0.0
                 else:
-                    gridval = _radial(z, n, l, r) * _ylm(l, m, dx, dy, dz, r)
+                    gridval = _radial(z, n, l, r) * _ylm(l, m, dx, dy, dz, r) * factor
                 cube[i, j, k] = gridval
